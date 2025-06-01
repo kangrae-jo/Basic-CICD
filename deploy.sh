5736f90cf81eb1842a4da6ea05944d6428a3678a
kangrae <kangrae0501@gmail.com>
2025-06-01 18:38:30 +0900

    chore: deploy 설정 변경

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APP_DIR=~/app/build/libs
-cd $APP_DIR || { echo "❌ 디렉토리 이동 실패"; exit 1; }
+cd $APP_DIR || { echo "디렉토리 이동 실패"; exit 1; }
 
 JAR_NAME=$(ls -t *.jar | grep -v plain | head -n 1)
 echo "[DEPLOY] 실행 대상: $JAR_NAME"
