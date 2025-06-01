0d96b12699b85e5b169a9eea18c8777b22258991
kangrae <kangrae0501@gmail.com>
2025-06-01 18:30:51 +0900

    chore: deploy.sh 변경

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 APP_DIR=~/app/build/libs
-cd $APP_DIR || { echo "디렉토리 이동 실패"; exit 1; }
+cd $APP_DIR || { echo "❌ 디렉토리 이동 실패"; exit 1; }
 
-# plain.jar 제외한 jar 중 가장 최신 것 실행
 JAR_NAME=$(ls -t *.jar | grep -v plain | head -n 1)
-
 echo "[DEPLOY] 실행 대상: $JAR_NAME"
 
-# 기존 프로세스 종료
 PID=$(pgrep -f $JAR_NAME)
 if [ -n "$PID" ]; then
   echo "[DEPLOY] 기존 프로세스 종료: $PID"
   kill -9 $PID
 fi
 
-# 실행
 echo "[DEPLOY] 앱 실행 시작"
-nohup java -jar $JAR_NAME > deploy.log 2>&1 &
+nohup java -jar $JAR_NAME > $APP_DIR/deploy.log 2>&1 &
