b9713df19a889719df5725d2bfc46a273fd2c7c6
kangrae <kangrae0501@gmail.com>
2025-06-04 15:19:05 +0900

    chore: ci시 test 결과 Comment 하는 설정 추가

---
+++ src/test/java/cicd/ApplicationTest.java
@@ -0,0 +1,19 @@
+package cicd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+    @Test
+    public void 테스트를_위한_테스트1() {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void 테스트를_위한_테스트2() {
+        assertEquals(2,2);
+    }
+
+}
